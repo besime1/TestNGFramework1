37d1568ca6f7ed2c09d9449ee1e3a6b3ee0c3794
besime1 <bsmdmr86@gmail.com>
2023-02-22 18:33:05 -0500

    first commit

---
+++ src/test/java/com/exelenter/testcases/AddEmployeeTest.java
@@ -0,0 +1,17 @@
+package com.exelenter.testcases;
+import com.exelenter.base.BaseClass;
+import com.exelenter.utils.ConfigsReader;
+import org.testng.annotations.Test;
+
+
+import static com.exelenter.base.PageInitializer.*;
+
+public class AddEmployeeTest extends BaseClass {
+    @Test
+    public void addEmployeeTest() {
+        loginPage.loginToWebsite(ConfigsReader.getProperties("username"),ConfigsReader.getProperties("password"));
+        pimPage.navigateToAddEmployee();
+        System.out.println("New Employee ID: " + addEmployeePage.employeeId.getAttribute("value"));
+        addEmployeePage.addEmployee("empFirstname", "empLastname", "filePath"); // This method will add a new employee
+    }
+}
